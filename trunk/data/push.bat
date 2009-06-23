593e8e2adc245453327329e4471e1f69732a2665
i25ffz <i25ffz@8907dee8-4f14-11de-b25e-a75f7371a613>
2009-06-23 09:29:56 +0000

    Push resources to Android emulator script.

---
+++ trunk/data/push.bat
@@ -0,0 +1,9 @@
+adb push tris.md2 /dev/sample/tris.md2
+adb push tris.bmp /dev/sample
+adb push weapon.md2 /dev/sample
+adb push weapon.bmp /dev/sample
+adb push font.bmp /dev/sample
+adb push floor.bmp /dev/sample
+adb push clouds.bmp /dev/sample
+
+pause
