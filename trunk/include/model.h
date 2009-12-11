de401984567bf602c62b3d48d4582f4ea277eef0
i25ffz <i25ffz@8907dee8-4f14-11de-b25e-a75f7371a613>
2009-12-11 09:35:14 +0000

    Support one model add more than one meshs feature.

--- trunk/include/model.h
+++ trunk/include/model.h
@@ -39,10 +39,10 @@
 
 namespace F3D {
     /**
-     * Model class for all games using F3D.
+     * Mesh class for all games using F3D.
      */
 
-    class Model {
+    class Mesh {
     protected:
         GLfloat     *m_vertices;
         GLfloat     *m_normals;
@@ -51,11 +51,50 @@ namespace F3D {
         GLshort     *m_indices;
         GLfloat     *m_position;
         GLfloat     *m_rotate;
+        GLfloat     *m_scale;
         GLint       m_textureId;
         GLint       m_triangleNums;
         GLboolean   m_enabled;
         //private functions
         void initGlCmds();
+    public:
+        /**
+        * Constructor
+        */
+        Mesh();
+
+        /**
+         * Destructor
+         */
+        virtual ~Mesh();
+
+        void setVertices(GLfloat *vertices, int size);
+        void setNormals(GLfloat *normals, int size);
+        void setUvs(GLfloat *uvs, int size);
+        void setColors(GLubyte *colors, int size);
+        void setIndices(GLshort *indices, int size);
+        void setTextureId(GLint textureId);
+        void setTriangleNums(GLint triangleNums);
+        void setEnabled(GLboolean enabled);
+        void setPosition(GLfloat x, GLfloat y, GLfloat z);
+        void setRotate(GLfloat x, GLfloat y, GLfloat z);
+        void setScale(GLfloat x, GLfloat y, GLfloat z);
+
+        /**
+         * Render mesh
+         */
+        void renderMesh();
+    };
+
+    /**
+     * Model class for all games using F3D.
+     */
+
+    class Model {
+    protected:
+        Mesh    *m_meshs;
+        int     m_meshCount;
+        //private functions
         virtual void prepareFrame();
     public:
         /**
@@ -68,17 +107,18 @@ namespace F3D {
          */
         virtual ~Model();
 
-        void setVertices(GLfloat *vertices);
-        void setNormals(GLfloat *normals);
-        void setUvs(GLfloat *uvs);
-        void setColors(GLubyte *colors);
-        void setIndices(GLshort *indices);
+        void setVertices(GLfloat *vertices, int size, int meshIndex = 0);
+        void setNormals(GLfloat *normals, int size, int meshIndex = 0);
+        void setUvs(GLfloat *uvs, int size, int meshIndex = 0);
+        void setColors(GLubyte *colors, int size, int meshIndex = 0);
+        void setIndices(GLshort *indices, int size, int meshIndex = 0);
         void setTextureId(GLint textureId);
-        void setTriangleNums(GLint triangleNums);
-        void setEnabled(GLboolean enabled);
+        void setTriangleNums(GLint triangleNums, int meshIndex = 0);
+        void setEnabled(GLboolean enabled, int meshIndex = 0);
         void setPosition(GLfloat x, GLfloat y, GLfloat z);
         void setRotate(GLfloat x, GLfloat y, GLfloat z);
-
+        void setScale(GLfloat x, GLfloat y, GLfloat z);
+        void setMeshCount(int meshCount);
         /**
          * Render model
          */
