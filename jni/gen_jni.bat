e40f57d5cde224ce82e125ef34c9d5f80760ba90
i25ffz <i25ffz@8907dee8-4f14-11de-b25e-a75f7371a613>
2010-12-05 13:27:40 +0000

    Delete old jni dir files.

--- jni/gen_jni.bat
+++
@@ -1,4 +0,0 @@
-javah -classpath bin;D:\android-sdk-windows\platforms\android-7\android.jar -jni com.forget3d.demo.F3DStarsRenderer
-javah -classpath bin;D:\android-sdk-windows\platforms\android-7\android.jar -jni com.forget3d.demo.F3DLightsRenderer
-
-pause
